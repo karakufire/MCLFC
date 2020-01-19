9db5ca0767de2dee6a5971627090d21090877aeb
karakufire <10906521+karakufire@users.noreply.github.com>
2020-01-19 21:47:58 +0900

    update langdiff.ts - add process of diff-ing

--- src/langdiff.ts
+++ src/langdiff.ts
@@ -1,10 +1,12 @@
 'use strict';
 
 import * as fs from 'fs';
+import * as path from 'path';
 import * as program from 'commander';
 import * as col from 'colors/safe';
 
 import * as errors from './errors';
+import { Entry, DiffLine } from './entry';
 
 const VERSION = '1.0.0';
 
@@ -27,6 +29,49 @@ try {
 
 function main() {
     if (program.args.length < 2) throw new errors.NotEnoughArgs('Not Enough Args: Args must be 2.');
-    if (!fs.existsSync(program.args[0])) throw new errors.NoSuchFile(program.args[0]);
-    if (!fs.existsSync(program.args[1])) throw new errors.NoSuchFile(program.args[1]);
+
+    const path_origin = program.args[0];
+    const path_trsltd = program.args[1];
+
+    if (!fs.existsSync(path_origin)) throw new errors.NoSuchFile(path_origin);
+    if (!fs.existsSync(path_trsltd)) throw new errors.NoSuchFile(path_trsltd);
+
+    const content_origin = fs.readFileSync(path_origin, 'utf-8');
+    const content_trsltd = fs.readFileSync(path_trsltd, 'utf-8');
+
+    const entries_origin = Entry.dropEmptyLines(Entry.parseToEntries(content_origin)).filter(e => !e.is_comment);
+    const entries_trsltd = Entry.dropEmptyLines(Entry.parseToEntries(content_trsltd)).filter(e => !e.is_comment);
+
+    let diffed = DiffLine.diff(entries_origin, entries_trsltd);
+    if (program['differences']) {
+        diffed = diffed.filter(e => e.isDifferentLine || e.hasEitherNullValue);
+    } else if (program['untranslated']) {
+        diffed = diffed.filter(e => e.hasEitherNullValue && (e.l2 < 0));
+    }
+
+    const header = (
+        `property name\t` +
+        `value of ${path.basename(path_origin)}\t` +
+        `line of ${path.basename(path_origin)}\t` +
+        `value of ${path.basename(path_trsltd)}\t` +
+        `line of ${path.basename(path_trsltd)}\t`
+    );
+
+    if (program['output']) {
+        const out = diffed.map(e => e.toString());
+        out.unshift(header);
+        const outPath = program['output'];
+        fs.writeFileSync(outPath, out.join('\n'));
+        console.log(`successfully output. see ${path.resolve(outPath)}`);
+    } else {
+        const out = diffed.map(e => {
+            if (e.isDifferentLine) return col.yellow(e.toString());
+            else if (e.hasEitherNullValue && e.l2 < 0) return col.bgRed(e.toString());
+            else if (e.hasEitherNullValue && e.l1 < 0) return col.bgGreen(e.toString());
+            else return e.toString();
+        });
+        out.unshift(col.inverse(header));
+        console.log(out.join('\n'));
+    }
+    return;
 }
